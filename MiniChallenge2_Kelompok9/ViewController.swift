b072bb87e001adc0c4fd52be380f113e0695d530
richardreynaldo <richardreynaldo626@gmail.com>
2020-05-06 21:55:03 +0700

    Initial Commit

---
+++ MiniChallenge2_Kelompok9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniChallenge2_Kelompok9
+//
+//  Created by Laurentius Richard Reynaldo on 06/05/20.
+//  Copyright © 2020 Laurentius Richard Reynaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MiniChallenge2_Kelompok9Tests/MiniChallenge2_Kelompok9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MiniChallenge2_Kelompok9Tests.swift
+//  MiniChallenge2_Kelompok9Tests
+//
+//  Created by Laurentius Richard Reynaldo on 06/05/20.
+//  Copyright © 2020 Laurentius Richard Reynaldo. All rights reserved.
+//
+
+import XCTest
+@testable import MiniChallenge2_Kelompok9
+
+class MiniChallenge2_Kelompok9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
