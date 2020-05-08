2184ee71d8f976058ef5ab6733d13aaef13c6ca0
Michael Geoferey <57284326+michaelgeoferey@users.noreply.github.com>
2020-05-08 12:39:24 +0700

    Coba Animasi Onboard

--- MiniChallenge2_Kelompok9/OnboardingVC.swift
+++ MiniChallenge2_Kelompok9/OnboardingVC.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class OnboardingVC: UIViewController {
 
+    @IBOutlet weak var imageOnboard1: UIImageView!
+    
+    @IBOutlet weak var imageOnboard2: UIImageView!
+ 
+    @IBOutlet weak var imageOnboard3: UIImageView!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
