98462dc78b24d36fb7513dd59724893657c16545
Michael Geoferey <57284326+michaelgeoferey@users.noreply.github.com>
2020-05-08 09:27:35 +0700

    Tambahan

---
+++ MiniChallenge2_Kelompok9/OnboardingVC.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingVC.swift
+//  MiniChallenge2_Kelompok9
+//
+//  Created by Michael Geoferey on 08/05/20.
+//  Copyright © 2020 Laurentius Richard Reynaldo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
