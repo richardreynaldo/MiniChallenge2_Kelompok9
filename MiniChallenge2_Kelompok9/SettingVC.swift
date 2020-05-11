3a67f246ea529bce78f49c1bf5d9232d9f214161
bryanzanr <bryanzanr@gmail.com>
2020-05-11 11:27:14 +0700

    Add setting layout template storyboard

---
+++ MiniChallenge2_Kelompok9/SettingVC.swift
@@ -0,0 +1,15 @@
+//
+//  SettingVC.swift
+//  MiniChallenge2_Kelompok9
+//
+//  Created by Bryanza on 11/05/20.
+//  Copyright © 2020 Laurentius Richard Reynaldo. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
