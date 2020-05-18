39f6e59c595e36cce04626550b24fea4f781f99c
Michael Geoferey <57284326+michaelgeoferey@users.noreply.github.com>
2020-05-18 15:27:01 +0700

    Overview
    swipe instruksi awal app

---
+++ MiniChallenge2_Kelompok9/Onboarding Folder/OnboardSlide.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardSlide.swift
+//  MiniChallenge2_Kelompok9
+//
+//  Created by Michael Geoferey on 18/05/20.
+//  Copyright © 2020 Laurentius Richard Reynaldo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardSlide: UIView {
+
+   
+    @IBOutlet weak var onboardLabel: UILabel!
+    @IBOutlet weak var onboardImage: UIImageView!
+    
+}
